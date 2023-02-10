69fcdac48e45dd4a5ee109a8493749b566a0f8d7
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2023-02-10 11:41:52 +0300

    Feat: add auto install enviroment

--- firmware_loader/flash.bat
+++ firmware_loader/flash.bat
@@ -1,4 +1,4 @@
 :: !!! YOU MUST TO INIT PYTHON ENVIROMENT AND LOAD ALL REQUIREMENTS !!!
 :: %1 - COM port of device, %2 - path to firmware
 :: Example - flash.bat COM6
-cmd /k "cd /d .venv\Scripts & activate & cd /d ..\.. & python w80xflash.py -p %1 -d ..\bin\w800\w800.img -r & exit"
+IF NOT EXIST .venv (python3 -m venv .venv & cmd /k "cd /d .venv\Scripts & activate & cd /d ..\.. & python -m pip install -r requirements.txt & python w80xflash.py -p %1 -d ..\bin\w800\w800.img -r & exit") ELSE (cmd /k "cd /d .venv\Scripts & activate & cd /d ..\.. & python w80xflash.py -p %1 -d ..\bin\w800\w800.img -r & exit")
